4190776057182375afb73a649dcf5dc53b2fa232
Pedro <pedro.zaluar@uniriotec.br>
2015-11-01 12:27:17 -0200

    Comit inicial Mancala: Tela do tabuleiro somente

---
+++ Mancala/app/src/main/java/br/com/uniriotec/mancala/JogoActivity.java
@@ -0,0 +1,23 @@
+package br.com.uniriotec.mancala;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class JogoActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jogo);
+	}
+
+	public void onClickButtonIniciar(View view) {
+	}
+
+	public void onClickButtonCavidade(View view) {
+	}
+}
